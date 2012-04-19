59341554fa061f7be6a09651ca4450feb666100f
Russ Thompson <russ@wildbit.com>
2012-04-19 14:04:35 -0400

    more functional

--- lemur.sh
+++ lemur.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 ## Lemur - Dat Dare Code
-##
+## Currently:  Debian/Ubuntu only.
 
-## Accept parms: dry, update
-##
+## Accept parms: dry, update, interactive
 
 LOG_FILE="rambo.log"
+LOG_PATH="log/"
 
 if [ -z "$1" ] ; then
    echo -e "Error -> no action supplied"
@@ -36,10 +37,22 @@ function run_updates {
          log "Preparing to update: $(echo $server | awk -F ":"'{print $2}')"
 	      run_remote "sudo apt-get update &> /dev/null ; echo $?" "repository update"
             if [ $? -eq "0" ] ; then
-               run_remote "sudo apt-get upgrade" "package upgrade" &> /tmp/return.txt
+               run_remote "sudo apt-get upgrade --show-upgraded --dry-run | grep "Inst" | awk '{print $2 " " $3}'" "package upgrade" &> /tmp/return.txt
             fi
        fi
    done
+}
+
+# Logic -> Initially generate a report of all packages installed.
+# We then add newly updated items to the report for tracking.
+function server_store {
+	C_SERV=`cat server.list | awk -F ":" '{print $1}') "$1" > /dev/null`
+	if [ -f "$LOG_PATH/$C_SERV" ] ; then
+		# Exists, proceed updating.
+	else
+		touch $LOG_PATH/$C_SERV
+		run_remote "dpkg -l | sed 's/ii//g'" "Initial package pull for $C_SERV" 
+	fi
 }
 
 run_updates
