50af7f3eea8c53a615cde3a28665c1bb16a2ceae
vigeek <vigeek>
2012-04-18 23:08:22 -0400

    second commit

--- lemur.sh
+++ lemur.sh
@@ -5,6 +5,7 @@
 ##
 
 LOG_FILE="rambo.log"
+
 if [ -z "$1" ] ; then
    echo -e "Error -> no action supplied"
    exit 1
@@ -25,15 +26,18 @@ function run_remote {
    retval=`ssh $(echo $server | awk -F ":" '{print $1}') "$1"`
       if [ "$retval" -eq "1" ] ; then
          log "Error -> [$2] failed on $(echo $server | awk -F ":" '{print $1}')"
+         return "1"
       fi
 }
 
 function run_updates {
    for server in `cat server.list` ; do
-      # ping -s 1 -c 1 $REMOTE > /dev/null; echo $?
        if [ "$(ping -s 1 -c 1 $(echo $server | awk  -F ":" '{print $2}') > /dev/null ; echo $?)" -eq "0" ] ; then
          log "Preparing to update: $(echo $server | awk -F ":"'{print $2}')"
 	      run_remote "sudo apt-get update &> /dev/null ; echo $?" "repository update"
+            if [ $? -eq "0" ] ; then
+               run_remote "sudo apt-get upgrade" "package upgrade" &> /tmp/return.txt
+            fi
        fi
    done
 }
