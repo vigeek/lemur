f878094af2756379176741be4e8606b25954bed3
vigeek <vigeek>
2012-04-26 12:34:17 -0400

    restructure, lemur is evil, do not use it

--- lemur.sh
+++ lemur.sh
@@ -2,11 +2,10 @@
 ## Lemur - Dat Dare Code
 ## Currently:  Debian/Ubuntu only.
 
-## Accept parms: dry, update, interactive
-
 LOG_FILE="rambo.log"
 LOG_PATH="log/"
 
+## Accept parms: dry, update, interactive
 if [ -z "$1" ] ; then
    echo -e "Error -> no action supplied"
    exit 1
@@ -23,6 +22,12 @@ function log {
    fi
 }
 
+function log_server {
+   if [ ! -z "$1" ] ; then
+       echo -e "$1" >> $SERVER_PACKAGES
+   fi
+}
+
 function run_remote {
    retval=`ssh $(echo $server | awk -F ":" '{print $1}') "$1"`
       if [ "$retval" -eq "1" ] ; then
@@ -32,27 +37,36 @@ function run_remote {
 }
 
 function run_updates {
-   for server in `cat server.list` ; do
-       if [ "$(ping -s 1 -c 1 $(echo $server | awk  -F ":" '{print $2}') > /dev/null ; echo $?)" -eq "0" ] ; then
-         log "Preparing to update: $(echo $server | awk -F ":"'{print $2}')"
-	      run_remote "sudo apt-get update &> /dev/null ; echo $?" "repository update"
-            if [ $? -eq "0" ] ; then
-               run_remote "sudo apt-get upgrade --show-upgraded --dry-run | grep "Inst" | awk '{print $2 " " $3}'" "package upgrade" &> /tmp/return.txt
-            fi
-       fi
-   done
+   server_store()
+   run_remote "sudo apt-get update &> /dev/null ; echo $?" "repository update"
+      if [ $? -eq "0"] ; then
+      else
+         log "Failure to update server."
+      fi
+
 }
 
 # Logic -> Initially generate a report of all packages installed.
 # We then add newly updated items to the report for tracking.
 function server_store {
-	C_SERV=`cat server.list | awk -F ":" '{print $1}') "$1" > /dev/null`
-	if [ -f "$LOG_PATH/$C_SERV" ] ; then
-		# Exists, proceed updating.
-	else
-		touch $LOG_PATH/$C_SERV
-		run_remote "dpkg -l | sed 's/ii//g'" "Initial package pull for $C_SERV" 
+	if [ -f "$SERVER_PACKAGES" ] ; then
+      log_server "----------------------------------------------------------"
+      log_server "$(date) - preparing to update..."
+      run_remote "sudo apt-get upgrade --show-upgraded --dry-run | grep "Inst" | awk '{print $2 " " $3}'" "package upgrade" >> $SERVER_PACKAGES
+   else
+		touch $LOG_PATH/$SERVER_HOST
+		run_remote "dpkg -l | sed 's/ii//g'" "Initial package pull for $C_SERV" > $SERVER_PACKAGES
 	fi
 }
 
-run_updates
+# This is our main loop function
+
+for server in `cat server.lst` ; do
+       if [ "$(ping -s 1 -c 1 $(echo $server | awk  -F ":" '{print $2}') > /dev/null ; echo $?)" -eq "0" ] ; then
+         SERVER_IP=`$(echo $server | awk -F ":"'{print $2}')`
+         SERVER_HOST=`$(echo $server | awk -F ":"'{print $1}')`
+         SERVER_PACKAGES=`$LOG_PATH/$SERVER_HOST/packages.lst`
+         log "Preparing to update: $SERVER_HOST"
+               server_store()
+       fi
+done
